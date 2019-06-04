734f1a680c824315f1586fbe18f3a95c825e9cdd
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-03 23:12:30 -0400

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,11 @@ burger_name VARCHAR (255) NOT NULL,
 devoured boolean,
 PRIMARY KEY (id)
 );
+
+-- separate file
+SET SQL_SAFE_UPDATES = 0;
+
+SELECT * FROM heroku_3b2e5fa92fe156f.burgers;
+
+update heroku_3b2e5fa92fe156f.burgers
+set devoured = 0;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Best Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Pickle Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', false);
+
+

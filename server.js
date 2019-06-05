02d9f5468c8f4a8af6af69767e9875a6b80580ce
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-05 10:24:52 -0400

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -23,7 +23,7 @@ const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(process.env.PORT || PORT, function() {
+app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });

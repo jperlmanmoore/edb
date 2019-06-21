7deca64987e0d7995e770519b484ee494a122092
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-21 16:42:16 -0400

    fixing

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,22 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-02.cleardb.net",
-//   port: 3310,
-//   user: "b2ec77dbeebfd3",
-//   password: "535f5494",
-//   database: "heroku_3b2e5fa92fe156f", 
-//   reconnect: true
-// });
-// ideally I would use the separate file for keeping this secret
-
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-02.cleardb.net",
   port: 3310,
-  user: "root",
-  password: "docker",
-  database: "burgers_db"
+  user: "b2ec77dbeebfd3",
+  password: "535f5494",
+  database: "heroku_3b2e5fa92fe156f", 
+  reconnect: true
 });
+
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3310,
+//   user: "root",
+//   password: "docker",
+//   database: "burgers_db"
+// });
 
 connection.connect(err => {
   if (err) {

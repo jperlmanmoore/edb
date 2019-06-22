acd1ea9a09a0eb3f4448e3fb6bbc44c4fbcb41e5
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-21 20:12:36 -0400

    delete

--- config/orm.js
+++ config/orm.js
@@ -88,6 +88,20 @@ var orm = {
 
       cb(result);
     });
+  },
+
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
   }
 };
 
